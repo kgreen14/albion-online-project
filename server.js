c0cacc078f4a155cc0ddc9b1e6f3072611e78ec5
kgreen14 <kgreen14@gmail.com>
2017-08-11 13:46:52 -0400

    added ItemController and app.us api/item

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ItemController = require("./controllers/item");
 const app = express();
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,9 +19,15 @@ connection.on('error', (err) => {
 }); 
 
 app.use(bodyParser.json());
+
+app.use('/api/item', ItemController)
+
+
 app.get('/', (req,res) => {
   res.send('Hello Albion!')
 })
+
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
